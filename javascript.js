08c2c49b7bbac806a973a93afc1291ce6aea1c0f
Ricky Arellano <80604395+aviskara@users.noreply.github.com>
2022-02-07 01:04:15 -0600

    change main
    change main layout and add style store

--- javascript.js
+++ javascript.js
@@ -8,7 +8,14 @@ function toggleTheme() {
     // to change the css sheet.
     if (theme.getAttribute('href') == 'css/main.css') {
         theme.setAttribute('href', 'css/style2.css');
+        localStorage.cssTemplate ='css/style2.css';
     } else {
         theme.setAttribute('href', 'css/main.css');
+        localStorage.cssTemplate = 'css/style2.css';
     }
 }
+
+window.addEventListener("load",function(){
+    var templateUrl = localStorage.cssTemplate || "css/main.css";
+    document.getElementsByTagName('link')[1].setAttribute("href",templateUrl);
+ });
